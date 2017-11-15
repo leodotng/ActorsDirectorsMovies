1fd268bc21100daa8de1f02a6bff74acbb3795aa
Josh <leong.jo@gmail.com>
2017-11-14 20:31:04 -0700

    addperson

--- routes/person.js
+++ routes/person.js
@@ -8,4 +8,17 @@ router.get("/", function(request, response, next) {
         response.render("person", {person: person});
     });
 });
+
+router.get("/new", function(request, response, next)  {
+    response.render("add_person");
+});
+
+router.post("/", function(request, response, next) {
+    knex("person").insert({
+        name: request.body.name,
+        role: request.body.role
+    }).then(function(){
+        response.redirect("/person");
+    });
+});
 module.exports = router;
